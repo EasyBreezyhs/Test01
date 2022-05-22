6665cfce6a2c18b2c8dc2e5804b82333205620d3
EasyBrezzy <1104511571@qq.com>
2022-05-22 21:17:09 +0800

    add meson and autoconf

---
+++ autoconf/hello/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "hello.h"
+
+using namespace std;
+
+int main()
+{
+    CHello a;
+    return 0;
+}
---
+++ autoconf/hello/hello.h
@@ -0,0 +1,14 @@
+#ifndef __HELLO_H__
+#define __HELLO_H__
+
+#include<iostream>
+using namespace std;
+
+class CHello
+{
+public:
+    CHello(){ cout<<"Hello!"<<endl;}
+    ~CHello(){ cout<<"Bye!"<<endl;}
+};
+
+#endif
